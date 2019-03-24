b6ca269647863168df7c466bd4d08cb378c6507a
mhashim6 <m6hashim@gmail.com>
2019-03-24 15:23:00 +0200

    preserve 'break' keyword.

--- src/main/kotlin/mhashim6/klox/TokenType.kt
+++ src/main/kotlin/mhashim6/klox/TokenType.kt
@@ -24,7 +24,7 @@ enum class TokenType {
     // Keywords.
     AND,
     CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
-    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE, BREAK,
 
     EOF
 }
